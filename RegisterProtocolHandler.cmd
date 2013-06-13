2a7d822197d91e824a09c9c32dfb6ab8b02e86c2
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-06-13 20:34:18 +0200

    Cleaned up register script

--- RegisterProtocolHandler.cmd
+++ RegisterProtocolHandler.cmd
@@ -3,19 +3,19 @@ SETLOCAL
 SET TPH_PATH=%~dp0
 SET TPH_PATH=%TPH_PATH:\=\\%
 
-ECHO Windows Registry Editor Version 5.00 > RegisterProtocolHandler.reg
-ECHO. >> RegisterProtocolHandler.reg
-ECHO [HKEY_CLASSES_ROOT\tel] >> RegisterProtocolHandler.reg
-ECHO "URL Protocol"="" >> RegisterProtocolHandler.reg
-ECHO @="TEL:Telephone Invocation" >> RegisterProtocolHandler.reg
-ECHO. >> RegisterProtocolHandler.reg
-ECHO [HKEY_CLASSES_ROOT\tel\shell] >> RegisterProtocolHandler.reg
-ECHO. >> RegisterProtocolHandler.reg
-ECHO [HKEY_CLASSES_ROOT\tel\shell\open] >> RegisterProtocolHandler.reg
-ECHO. >> RegisterProtocolHandler.reg
-ECHO [HKEY_CLASSES_ROOT\tel\shell\open\command] >> RegisterProtocolHandler.reg
-ECHO @="\"%TPH_PATH%TelProtocolHandler\\bin\\Release\\TelProtocolHandler.exe\" \"%%1\"" >> RegisterProtocolHandler.reg
-ECHO. >> RegisterProtocolHandler.reg
+ >RegisterProtocolHandler.reg ECHO Windows Registry Editor Version 5.00
+>>RegisterProtocolHandler.reg ECHO.
+>>RegisterProtocolHandler.reg ECHO [HKEY_CLASSES_ROOT\tel]
+>>RegisterProtocolHandler.reg ECHO "URL Protocol"=""
+>>RegisterProtocolHandler.reg ECHO @="TEL:Telephone Invocation"
+>>RegisterProtocolHandler.reg ECHO.
+>>RegisterProtocolHandler.reg ECHO [HKEY_CLASSES_ROOT\tel\shell]
+>>RegisterProtocolHandler.reg ECHO.
+>>RegisterProtocolHandler.reg ECHO [HKEY_CLASSES_ROOT\tel\shell\open]
+>>RegisterProtocolHandler.reg ECHO.
+>>RegisterProtocolHandler.reg ECHO [HKEY_CLASSES_ROOT\tel\shell\open\command]
+>>RegisterProtocolHandler.reg ECHO @="\"%TPH_PATH%TelProtocolHandler\\bin\\Release\\TelProtocolHandler.exe\" \"%%1\""
+>>RegisterProtocolHandler.reg ECHO.
 
 start RegisterProtocolHandler.reg
 
