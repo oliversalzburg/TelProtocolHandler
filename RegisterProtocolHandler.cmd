b398b5d502539535d8e03c5ceb3c40ee9f042395
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-06-13 18:32:09 +0200

    Added script to create protocol handler registry entries

---
+++ RegisterProtocolHandler.cmd
@@ -0,0 +1,22 @@
+@ECHO OFF
+SETLOCAL
+SET TPH_PATH=%~dp0
+SET TPH_PATH=%TPH_PATH:\=\\%
+
+ECHO Windows Registry Editor Version 5.00 > RegisterProtocolHandler.reg
+ECHO. >> RegisterProtocolHandler.reg
+ECHO [HKEY_CLASSES_ROOT\tel] >> RegisterProtocolHandler.reg
+ECHO "URL Protocol"="" >> RegisterProtocolHandler.reg
+ECHO @="TEL:Telephone Invocation" >> RegisterProtocolHandler.reg
+ECHO. >> RegisterProtocolHandler.reg
+ECHO [HKEY_CLASSES_ROOT\tel\shell] >> RegisterProtocolHandler.reg
+ECHO. >> RegisterProtocolHandler.reg
+ECHO [HKEY_CLASSES_ROOT\tel\shell\open] >> RegisterProtocolHandler.reg
+ECHO. >> RegisterProtocolHandler.reg
+ECHO [HKEY_CLASSES_ROOT\tel\shell\open\command] >> RegisterProtocolHandler.reg
+ECHO @="\"%TPH_PATH%bin\\Release\\TelProtocolHandler.exe\" \"%%1\"" >> RegisterProtocolHandler.reg
+ECHO. >> RegisterProtocolHandler.reg
+
+start RegisterProtocolHandler.reg
+
+ENDLOCAL
