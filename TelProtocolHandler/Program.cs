ebbc299e80e08e376bd73a80f5e8138f5dbe709e
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-06-13 18:14:08 +0200

    Initital commit

---
+++ TelProtocolHandler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace TelProtocolHandler {
+  internal class Program {
+    private static void Main( string[] args ) {
+      if( args.Length < 1 ) {
+        Console.WriteLine( "No arguments given." );
+      } else {
+        Console.WriteLine( "Calling '{0}'...", args[ 0 ] );
+      }
+
+      Thread.Sleep( TimeSpan.FromSeconds( 5.0 ) );
+    }
+  }
+}
