be435c6a0326f32ecf09869d3adc258818f46ce0
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-06-13 18:42:28 +0200

    Cleaning up input

--- TelProtocolHandler/Program.cs
+++ TelProtocolHandler/Program.cs
@@ -3,11 +3,22 @@ using System.Threading;
 
 namespace TelProtocolHandler {
   internal class Program {
+    private static string Protocol = "tel:";
+
     private static void Main( string[] args ) {
       if( args.Length < 1 ) {
         Console.WriteLine( "No arguments given." );
       } else {
-        Console.WriteLine( "Calling '{0}'...", args[ 0 ] );
+        string phoneNumberInput = args[ 0 ];
+
+        if( !phoneNumberInput.StartsWith( Protocol ) ) {
+          Console.WriteLine( string.Format( "Unexpected input. Expected argument to start with '{0}'.", Protocol ) );
+          return;
+        }
+
+        string phoneNumber = phoneNumberInput.Substring( Protocol.Length );
+
+        Console.WriteLine( "Calling '{0}'...", phoneNumber );
       }
 
       Thread.Sleep( TimeSpan.FromSeconds( 5.0 ) );
