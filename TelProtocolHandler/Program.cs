b1ce3082e45441759b4338d1c761aedc6436f534
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-02-25 10:54:00 +0100

    Can now handle more phone number formats and different line names

--- TelProtocolHandler/Program.cs
+++ TelProtocolHandler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using JulMar.Tapi3;
@@ -25,26 +26,48 @@ namespace TelProtocolHandler {
         Console.WriteLine(currentProcess.MainWindowTitle);
 
         IntPtr hWnd = currentProcess.MainWindowHandle;
-        if (hWnd != IntPtr.Zero) {
+        if( !Debugger.IsAttached && hWnd != IntPtr.Zero ) {
           //Hide the window
-          ShowWindow(hWnd, 0); // 0 = SW_HIDE
+          ShowWindow( hWnd, 0 ); // 0 = SW_HIDE
         }
 
         string phoneNumber = phoneNumberInput.Substring( Protocol.Length );
 
         // TODO: Replace with configuration value
-        const string lineToUse = "Obelix (Aastra5370/5370ip)";
+        string lineToFind = "Aastra";
+        string lineToUse = null;
 
         TTapi tapi = new TTapi();
-        int foundDevices = tapi.Initialize();
-        Console.WriteLine( "{0} devices found", foundDevices );
-        foreach( TAddress addr in tapi.Addresses ) {
-          Console.WriteLine( addr.AddressName );
+        if( !string.IsNullOrEmpty( lineToFind ) ) {
+          int foundDevices = tapi.Initialize();
+          Console.WriteLine( "{0} devices found", foundDevices );
+          foreach( TAddress addr in tapi.Addresses ) {
+            Console.WriteLine( addr.AddressName );
+            if( -1 < addr.AddressName.IndexOf( lineToFind ) ) {
+              lineToUse = addr.AddressName;
+            }
+          }
+        }
+
+        if( string.IsNullOrEmpty( lineToUse ) ) {
+          Console.WriteLine( "Unable to find suitable TAPI line!" );
+          return;
+        }
+
+        // Replace + prefix with a 00
+        if( phoneNumber.StartsWith( "+" ) ) {
+          phoneNumber = "00" + phoneNumber.Substring( 1 );
         }
 
         TAddress line = tapi.Addresses.SingleOrDefault(a => a.AddressName == lineToUse);
-        TCall call = line.CreateCall("0" + phoneNumber, LINEADDRESSTYPES.PhoneNumber, TAPIMEDIATYPES.AUDIO);
-        call.Connect(false);
+        if( null == line ) {
+          Console.WriteLine( "Unable to find TAPI line with name '{0}'!", lineToUse );
+          return;
+        }
+
+        // Always assumes 0 prefix is needed to dial out.
+        TCall call = line.CreateCall( "0" + phoneNumber, LINEADDRESSTYPES.PhoneNumber, TAPIMEDIATYPES.AUDIO );
+        call.Connect( false );
 
         Console.WriteLine( "Calling '{0}'...", phoneNumber );
       }
