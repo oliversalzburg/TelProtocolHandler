92c2732fe32ff2aef0913801bfae7545cf245772
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-12-02 15:52:39 +0100

    [TASK] Cleaned up handler setup

--- TelProtocolHandlerSetup/Program.cs
+++ TelProtocolHandlerSetup/Program.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 
 namespace TelProtocolHandlerSetup {
-    class Program {
-        static void Main (string[] args) {
-            string registryKey = @"HKEY_CLASSES_ROOT\tel";
-            string registryValue = "TEL:Telephone Invocation";
-            Microsoft.Win32.Registry.SetValue(registryKey, string.Empty, registryValue, Microsoft.Win32.RegistryValueKind.String);
-            Microsoft.Win32.Registry.SetValue(registryKey, "URL Protocol", String.Empty, Microsoft.Win32.RegistryValueKind.String);
+    static class Program {
+        static void Main( string[] args ) {
+            const string protocolValue = "TEL:Telephone Invocation";
+            Registry.SetValue( @"HKEY_CLASSES_ROOT\tel", string.Empty, protocolValue, RegistryValueKind.String );
+            Registry.SetValue( @"HKEY_CLASSES_ROOT\tel", "URL Protocol", String.Empty, RegistryValueKind.String );
 
-            registryKey = @"HKEY_CLASSES_ROOT\tel\shell\open\command";
-            registryValue = "\"" + AppDomain.CurrentDomain.BaseDirectory + "TelProtocolHandler.exe\" \"%1\"";
-            Microsoft.Win32.Registry.SetValue(registryKey, string.Empty, registryValue, Microsoft.Win32.RegistryValueKind.String);
+            string binaryName = AppDomain.CurrentDomain.FriendlyName;
+            string command = string.Format( "\"{0}{1}\" \"%1\"", AppDomain.CurrentDomain.BaseDirectory, binaryName );
+            Registry.SetValue( @"HKEY_CLASSES_ROOT\tel\shell\open\command", string.Empty, command, RegistryValueKind.String );
         }
     }
 }
