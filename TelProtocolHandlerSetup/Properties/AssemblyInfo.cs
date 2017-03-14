308bc34063876d99bb34c22a2a00f11bb0628988
Oliver Salzburg <oliver.salzburg@gmail.com>
2017-03-14 12:01:42 +0100

    [TASK] Code style

--- TelProtocolHandlerSetup/Properties/AssemblyInfo.cs
+++ TelProtocolHandlerSetup/Properties/AssemblyInfo.cs
@@ -1,26 +1,28 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle( "tel: Protocol Handler Setup")]
+
+[assembly: AssemblyTitle( "tel: Protocol Handler Setup" )]
 [assembly: AssemblyDescription( "tel: Protocol Handler Setup" )]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TelProtocolHandlerSetup")]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "TelProtocolHandlerSetup" )]
 [assembly: AssemblyCopyright( "Copyright © Oliver Salzburg 2014-2017" )]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
 // Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
 // für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
 // COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
-[assembly: ComVisible(false)]
+
+[assembly: ComVisible( false )]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("4848684b-9952-4dfb-acc8-dcad07d52b10")]
+
+[assembly: Guid( "4848684b-9952-4dfb-acc8-dcad07d52b10" )]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
@@ -32,5 +34,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion( "3.1.0.0" )]
 [assembly: AssemblyFileVersion( "3.1.0.0" )]
